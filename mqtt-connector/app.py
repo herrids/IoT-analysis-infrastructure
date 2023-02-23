95f2dc89a47f148eff1abe44fb0c998263abdff3
Ferdi <hoskeee@gmail.com>
2023-02-23 16:04:15 +0100

    changed back hose of cassandra

--- mqtt-connector/app.py
+++ mqtt-connector/app.py
@@ -25,7 +25,7 @@ def receive():
 
 def connect_db():
     auth_provider = PlainTextAuthProvider(username='cassandra', password='cassandra')
-    cluster = Cluster(['127.0.0.1'], port=9042, auth_provider = auth_provider)
+    cluster = Cluster(['cassandra'], port=9042, auth_provider = auth_provider)
     session = cluster.connect(wait_for_all_pools=True)
     return session
 
