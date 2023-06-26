3fc081d07189891a55c3301d6b9d0ee961c6f9ea
Ferdi <hoskeee@gmail.com>
2023-06-26 23:35:16 +0200

    error handler cassandra

--- kafka-streams-app/src/main/java/unipotsdam/myno/CassandraDao.java
+++ kafka-streams-app/src/main/java/unipotsdam/myno/CassandraDao.java
@@ -1,8 +1,6 @@
 package unipotsdam.myno;
 
 import com.datastax.oss.driver.api.core.CqlSession;
-import com.datastax.oss.driver.api.core.cql.ResultSet;
-import com.datastax.oss.driver.api.core.cql.Row;
 
 public class CassandraDao {
     private final CqlSession session;
@@ -12,19 +10,18 @@ public class CassandraDao {
     }
 
     public void saveSensorData(String sensorType, String sensorNumber, String boardUuid, String timestamp, double value) {
-        // check if table exists
-        String checkTableQuery = String.format("SELECT table_name FROM system_schema.tables WHERE keyspace_name = 'myno' AND table_name = 'sensor_%s';", sensorType);
-        ResultSet resultSet = session.execute(checkTableQuery);
-        Row row = resultSet.one();
-        
-        // if table doesn't exist, create it
-        if (row == null) {
-            String createTableQuery = String.format("CREATE TABLE sensor_%s (sensornumber text, board_uuid text, timestamp timestamp, sensorvalue double, PRIMARY KEY (sensornumber, timestamp));", sensorType);
+        try {
+            // if table doesn't exist, create it
+            String createTableQuery = String.format("CREATE TABLE IF NOT EXISTS sensor_%s (sensornumber text, board_uuid text, timestamp timestamp, sensorvalue double, PRIMARY KEY (sensornumber, timestamp));", sensorType);
             session.execute(createTableQuery);
+    
+            // insert data
+            String insertDataQuery = String.format("INSERT INTO sensor_%s (sensornumber, board_uuid, timestamp, sensorvalue) VALUES (?, ?, ?, ?);", sensorType);
+            session.execute(insertDataQuery, sensorNumber, boardUuid, timestamp, value);
+        } catch (com.datastax.oss.driver.api.core.DriverException e) {
+            // Handle the exception. Maybe log it, or send a message somewhere notifying you of the issue.
+            e.printStackTrace();
         }
-
-        // insert data
-        String insertDataQuery = String.format("INSERT INTO sensor_%s (sensornumber, board_uuid, timestamp, sensorvalue) VALUES (?, ?, ?, ?);", sensorType);
-        session.execute(insertDataQuery, sensorNumber, boardUuid, timestamp, value);
     }
+    
 }
