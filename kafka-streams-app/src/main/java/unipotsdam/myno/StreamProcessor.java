221e8bc8d479efb1506082b900636254d128c6a2
Ferdi <hoskeee@gmail.com>
2023-07-09 19:05:05 +0200

    return stream

--- kafka-streams-app/src/main/java/unipotsdam/myno/StreamProcessor.java
+++ kafka-streams-app/src/main/java/unipotsdam/myno/StreamProcessor.java
@@ -5,13 +5,19 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -48,13 +54,11 @@ public class StreamProcessor {
         KStream<String, String> source = builder.stream(INPUT_TOPIC);
 
         // Process each record in the stream
-        source.foreach((key, value) -> {
+        KStream<String, SensorData> sensorDataStream = source.mapValues(value -> {
             ObjectMapper objectMapper = new ObjectMapper();
-            SensorData sensorData;
             try {
-                logger.debug(value);
                 // Deserialize the JSON value into a SensorData object
-                sensorData = objectMapper.readValue(value, SensorData.class);
+                SensorData sensorData = objectMapper.readValue(value, SensorData.class);
 
                 // Create a table for the sensor type if it doesn't exist
                 dao.createTableIfNotExists("sensor_" + sensorData.getSensorType(), "CREATE TABLE IF NOT EXISTS %s (sensor_number int, board_uuid text, timestamp timestamp, sensor_value double, PRIMARY KEY (sensor_number, timestamp));");
@@ -88,11 +92,49 @@ public class StreamProcessor {
                     (float) stats.getMean(),
                     (float) stats.getMedian()
                 );
+
+                return sensorData;
             } catch (JsonProcessingException e) {
                 logger.error("An error occurred while processing stream", e);
+                return null;
             }
         });
 
+
+
+         // Create a key for each record based on sensorType and sensorNumber
+        KStream<String, SensorData> sensorDataStreamWithKey = sensorDataStream.selectKey((k, v) -> v.getSensorType() + "_" + v.getSensorNumber() + "_" + v.getBoardUuid() + "_" + LocalDate.now());
+        /*
+        sensorDataStreamWithKey.groupByKey()
+                .windowedBy(TimeWindows.of(Duration.ofDays(1)).grace(Duration.ofHours(1)))
+                .aggregate(
+                    SensorDataStatistics::new, // Initializer
+                    (key, value, aggregate) -> aggregate.updateWith(value), // Aggregator
+                    Materialized.with(Serdes.String(), new JsonPOJOSerde<SensorDataStatistics>())
+                )
+                .toStream()
+                .foreach((key, value) -> {
+                    String[] parts = key.key().split("_");
+                    String sensorType = parts[0];
+                    int sensorNumber = Integer.parseInt(parts[1]);
+                    String boardUuid = parts[2];
+
+                    // Extract date from window start
+                    LocalDate date = Instant.ofEpochMilli(key.window().start()).atZone(ZoneId.systemDefault()).toLocalDate();
+
+                    // Save statistics to the database
+                    dao.saveSensorStatistics(
+                        sensorType,
+                        sensorNumber,
+                        boardUuid,
+                        date,
+                        (float) value.getMin(),
+                        (float) value.getMax(),
+                        (float) value.getMean(),
+                        (float) value.getMedian()
+                    );
+                });
+         */
         // Create a Kafka Streams object and start the processing
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.start();
